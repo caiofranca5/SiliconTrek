293467a1413bfcbc4b54a9568a5abf88c90994db
Caio França <caiofranca5@hotmail.com>
2024-04-02 15:13:02 -0300

    Initial Commit

---
+++ ProjectSilicon/ProjectSiliconApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectSiliconApp.swift
+//  ProjectSilicon
+//
+//  Created by Caio França on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectSiliconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
